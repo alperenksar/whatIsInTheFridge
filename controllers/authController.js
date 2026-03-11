4faca7e75870b0bba09cb2203389456a5c92228e
Alperen Kasar <alperenasd@gmail.com>
2026-03-11 07:00:47 +0000

    Login
    Login API is workinggg!

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,53 @@
 const {query} = require("../config/database");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+
+const login = async(req ,res) => {
+    const {username , password}=req.body;
+
+    try{
+        const result = await query('SELECT * FROM users WHERE username = $1' , [username]);
+
+        if(result.rows.length === 0){
+            return res.status(401).json({error:"Wrong username"});
+        }
+
+        const user = result.rows[0];
+
+        const isTrue = await bcrypt.compare(password , user.password);
+        if(!isTrue){
+            return res.status(401).json({error:"Wrong password"});
+        }
+
+        const token = jwt.sign({
+            id : user.id,
+            username : user.username
+        },
+        process.env.JWT_SECRET,{
+            expiresIn:'24h'
+        });
+
+        res.json({
+            message:"Login succesful",
+            token:token
+        });
+
+
+    }
+
+    catch(err){
+        console.log(err);
+        res.status(500).json({error:"Not successful"});
+    }
+
+
+}
+
+
+
+
 
 const register = async (req,res) => {
     const {username , password} = req.body;
@@ -9,7 +57,7 @@ const register = async (req,res) => {
 
         const newUser = await query(
             'INSERT INTO users (username, password) VALUES ($1,$2) RETURNING id, username',
-            [username, hashedPassword] 
+            [username, cryptedPassword] 
         );
 
         res.status(201).json({
@@ -22,4 +70,4 @@ const register = async (req,res) => {
     }
 };
 
-module.exports = {register};
+module.exports = {register ,login};
