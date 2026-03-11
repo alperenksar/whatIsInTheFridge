6a757383505175e0937274d1db61f4091d93d573
Alperen Kasar <alperenasd@gmail.com>
2026-03-11 11:35:05 +0000

    Items can be deleting with their name's
    It is not dynamic at the moment , just deleting which name's "Milk"

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+<<<<<<< Updated upstream
 const { addItem, getItems } = require('../controllers/itemController');
+=======
+const { addItem, getItems  } = require('../controllers/itemController');
+const { deleteItem } = require('../controllers/itemController');
+>>>>>>> Stashed changes
 
 
 
 router.post('/addItem', addItem);
 router.get('/getItems', getItems);
+router.delete('/deleteItem', deleteItem);
 
 module.exports = router;
